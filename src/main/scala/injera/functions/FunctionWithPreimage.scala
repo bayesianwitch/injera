f49e8fb6f5e1fe1f8eeb5085e1893cae56e82592
Chris Stucchio <stucchio@gmail.com>
2014-01-12 08:58:20 -0500

    Created functionM version

--- src/main/scala/injera/functions/FunctionWithPreimage.scala
+++ src/main/scala/injera/functions/FunctionWithPreimage.scala
@@ -1,10 +1,18 @@
 package com.bayesianwitch.injera.functions
 
 import scalaz._
+import Scalaz._
 
-trait FunctionWithPreimage[K,V] extends (K => V) {
+trait ComputesPreimage[K,-V] {
   def preimage(v: V): NonEmptyList[K]
   def partialInverse(v: V): K = preimage(v).head
+}
+
+trait FunctionWithPreimage[K,V] extends (K => V) with ComputesPreimage[K,V]
+
+trait FunctionMWithPreimage[K,V,M[_]] extends (K => M[V]) with ComputesPreimage[K,V] {
+  protected implicit val mApplicative: Applicative[M]
+  def preimageM(v: V): M[NonEmptyList[K]] = v.point[M].map(preimage)
 }
 
 trait HasFunctionWithPreimage[K,-V] {
