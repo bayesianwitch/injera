923b3a9da53a5db8ca913373355179d6542e89b0
Chris Stucchio <stucchio@gmail.com>
2014-02-02 23:53:22 +0530

    Created random utils package

---
+++ src/main/scala/injera/random/package.scala
@@ -0,0 +1,27 @@
+package com.bayesianwitch.injera
+
+import scala.util.Random
+
+package object random {
+
+  implicit class WeightedSeq[T](val seq: Seq[(T,Double)]) extends AnyVal {
+    def weightedChoice: T = {
+      val totalWeight = seq.map(_._2).sum
+      val rnd = Random.nextDouble * totalWeight
+      var i: Int = 0
+      var cumsum: Double = 0
+      var result: Option[T] = None
+      while ((i < seq.size) && result.isEmpty) {
+        if (seq(i)._2 <= 0) {
+          throw new IllegalArgumentException("Cannot have negative weight.")
+        }
+        cumsum += seq(i)._2
+        if ((cumsum > rnd) || (i == seq.size-1)) {
+          result = Some(seq(i)._1)
+        }
+        i += 1
+      }
+      result.get
+    }
+  }
+}
