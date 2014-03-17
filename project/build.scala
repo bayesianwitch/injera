a6d0d4434d31024baa59507d32646d0639e13057
Chris Stucchio <stucchio@gmail.com>
2014-03-17 15:44:25 +0530

    increment version number

--- project/build.scala
+++ project/build.scala
@@ -8,7 +8,7 @@ object ApplicationBuild extends Build {
     organization := "com.bayesianwitch",
     credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
     scalaVersion := "2.10.0",
-    version := "0.01.5",
+    version := "0.01.6",
     resolvers ++= myResolvers,
     name := "injera",
     //fork := true,
